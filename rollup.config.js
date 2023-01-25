ac3648e84dc7188e6232236c4e3ff0389b9f254e
Abd Alhamid <hamid@sprintive.com>
2023-01-25 16:51:47 +0300

    attempt to fix node-gyp

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,44 @@
 import filesize from "rollup-plugin-filesize";
 import { eslint } from "rollup-plugin-eslint";
-import babel from "rollup-plugin-babel";
-import commonjs from "rollup-plugin-commonjs";
-import resolve from "rollup-plugin-node-resolve";
+import babel from "@rollup/plugin-babel";
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import replace from "@rollup/plugin-replace";
 import sass from "rollup-plugin-sass";
-import { uglify } from "rollup-plugin-uglify";
+import { terser } from "rollup-plugin-terser";
 import pkg from "./package.json";
 
+const isDev = Boolean(process.argv.find((arg) => arg === "--configDev"));
+
+const babelplugin = babel({
+  babelHelpers: "bundled",
+  exclude: pkg.commonjs
+});
+const commonjsplugin = commonjs({
+  exclude: "node_modules/process-es6/**",
+  include: pkg.commonjs
+});
+const replaceplugin = replace({
+  preventAssignment: true,
+  "process.env.NODE_ENV": isDev
+    ? JSON.stringify("development")
+    : JSON.stringify("production"),
+});
+const resolveplugin = resolve({
+  mainFields: ["module", "jsnext:main", "browser"],
+  browser: true,
+  preferBuiltins: true
+});
+const sassplugin = sass({ insert: false });
+const plugins = [
+  sassplugin,
+  resolveplugin,
+  commonjsplugin,
+  replaceplugin,
+  babelplugin,
+  filesize(),
+]
+// eslint-disable-next-line import/no-anonymous-default-export
 export default [{
   input: pkg.src,
   output: {
@@ -17,20 +49,12 @@ export default [{
   },
   plugins: [
     eslint({
+      fix: true,
       include: [
         "./src/**",
       ]
     }),
-    sass({ output: "tourguide.css", options: { outputStyle: "compressed" } }),
-    resolve({ mainFields: ["module", "jsnext:main", "browser"], browser: true, preferBuiltins: true }),
-    commonjs({
-      exclude: "node_modules/process-es6/**",
-      include: pkg.commonjs
-    }),
-    babel({
-      exclude: pkg.commonjs
-    }),
-    filesize(),
+    ...plugins
   ]
 }, {
   input: pkg.main,
@@ -38,22 +62,20 @@ export default [{
     format: "esm",
     file: pkg.main.replace(".js", ".min.js")
   },
-  plugins: [uglify()]
+  plugins: [terser()]
 }, {
   input: pkg.src,
   output: {
     format: "esm",
     file: pkg.module
   },
-  plugins: [
-    sass({ insert: false }),
-    resolve({ mainFields: ["module", "jsnext:main", "node"], browser: true, preferBuiltins: true }),
-    commonjs({
-      exclude: "node_modules/process-es6/**",
-      include: pkg.commonjs
-    }),
-    babel({
-      exclude: pkg.commonjs
-    })
-  ]
+  plugins: plugins
+}, {
+  input: pkg.src,
+  output: {
+    format: "umd",
+    "name": "Tourguide",
+    file: pkg.module.replace(".esm.js", ".umd.js")
+  },
+  plugins: plugins
 }];
